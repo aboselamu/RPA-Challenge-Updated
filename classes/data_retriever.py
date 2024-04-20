d25c0dd6eddec55a9b6e73542190ac06306e89db
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-20 08:54:13 +0200

    Update data_retriever.py

--- classes/data_retriever.py
+++ classes/data_retriever.py
@@ -14,6 +14,7 @@ class DataRetriever:
         dp = DataProcessor()
         
         browser = self.browser_manager.browser
+        # article counter
         counter = 1
         
         # Handling the possible inputs
@@ -27,22 +28,22 @@ class DataRetriever:
         # to store articles for extraction
         articles_titiles = []    
         try:
-            # browser.wait_until_element_is_visible("xpath://*[@id='main-content-area']/div[2]/div[2]", timeout=10)
+            # xpath of he element //*[@id='main-content-area']/div[2]/div[2]"
             # Wait for the element to be visible with a timeout
             browser.wait_until_element_is_visible('css:.search-result__list', timeout=timedelta(seconds=10))
             
         except Exception as e:
             pass
     
-            # to handle paggination
+        # to handle paggination
         is_there_ShowMore = True
             
         while is_there_ShowMore:
-            print("Inside while loop")
+
             # Search result section
             search_list_selector = browser.find_element(locator="css:#main-content-area > div.l-col.l-col--8 > div.search-result__list")
-            # search_list_selector = browser.find_element(locator="css:.search-result__list")     
-            #browser.find_element("xpath=//*[@id='main-content-area']/div[2]/div[2]")
+            # search_list_selector = browser.find_element(locator="css:.search-result__list") 
+            
             articles = browser.find_elements("tag:article", parent=search_list_selector)
 
             # the show more button
@@ -50,34 +51,36 @@ class DataRetriever:
 
             # for each articles 
             for article in articles:
-                print("inside article for loop") 
+                
                 # getting excert section
                 excert = browser.find_element("tag:p",parent=article)
-                print("after excert")
+
                 # getting time and description of the post from excert
                 time_of_post, description  = dp.extract_before_ellipsis(excert.text)
-                print(time_of_post, description, "check here")
+
                 try:
                     article_date = dp.formated_article_date(time_of_post)
                 except Exception as e:
-                    print(e, "article, date format")
-                print("after article date")
+                    pass
+                    
                 # check if the artices does contains date
-                # if(article_date == None):
-                #     continue
+                if(article_date == None):
+                    continue
                 try:
 
                     # checking the article date is in the time period of the input
                     if dp.is_within_time_frame(article_date, target_date):
                         title= browser.find_element("tag:h3", parent=article)
+                        
                         if title.text not in articles_titiles:
                             articles_titiles.append(title.text)
                             
                             # does the title or description contains money
                             # checking how many times the search keyword apears in title and description
                             no_of_search_phrase, contains = dp.no_of_topic_and_money_amount(title.text, 
-                                                                                          description, 
-                                                                                          search_phrase)
+                                                                                             description, 
+                                                                                              search_phrase
+                                                                                            )
                             # finding the imgae of each article
                             image = browser.find_element(locator="tag:img", parent=article)
                             image_url = image.get_attribute('src')
@@ -85,25 +88,20 @@ class DataRetriever:
                             picture_name = image_url.split("/")[-1]  # Extracting picture name from URL
                             output_path = Path(get_output_dir()) / picture_name
     
-                            ready_article = {"No":counter, "Title": title.text, "Date": article_date, 
-                                             "Description": description, "PictureFilename": picture_name, 
-                                             "Count": no_of_search_phrase, "ContainsMoney": contains
-                                                }
+                            ready_article = {"No":counter, "Title": title.text, 
+                                              "Date": article_date, "Description": description, 
+                                              "PictureFilename": picture_name, "Count": no_of_search_phrase, 
+                                              "ContainsMoney": contains
+                                            }
     
                             # Making work items to be saved on file
-                            # for article in articles:
                             workitems.outputs.create(payload=ready_article)
-                            print("work item created")
-
-    
-    
-                            # data.append([counter,title.text, article_date, description, 
-                            #                     picture_name, no_of_search_phrase, contains])
-                            #update counter
+                            
+                            # updating the article counter
                             counter+=1
     
                 except Exception as e:
-                    print(e, "try to put everything")
+                    pass
     
             # try to locate and close the ads section
             try:
@@ -111,7 +109,6 @@ class DataRetriever:
                 browser.click_button(ads_locator) 
     
             except Exception as e:
-                print("no ads locarion")
                 pass
             
             # Trying to find if there is more article
@@ -120,14 +117,10 @@ class DataRetriever:
                 browser.scroll_element_into_view(button_locator)
                 browser.wait_until_element_is_enabled(button_locator, timeout=10)
     
-    
                 browser.click_button(button_locator)
                 time.sleep(5)
-                print("Botton Clicked")
         
             except Exception as e: 
                 is_there_ShowMore = False
-                print("no button clicked")
-                pass
 
 
