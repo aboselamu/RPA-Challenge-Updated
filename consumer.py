4514d2bfbfa73b057668292822a32401114cdfa7
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-21 06:24:21 +0200

    formatting to pep 8 style

--- consumer.py
+++ consumer.py
@@ -1,43 +1,46 @@
-from RPA.Browser.Selenium import Selenium 
-import re
-import time
-import requests
+"""
+This module handles the consumer logic.
+It process the payload produced by producer.
+"""
 import logging
 from pathlib import Path
-from robocorp import vault
 from robocorp import excel
-from robocorp import storage
-from datetime import datetime
 from robocorp.tasks import task
 from robocorp import workitems
-# from robocorp.workitems import WorkItems
-from datetime import datetime, timedelta
 from robocorp.tasks import get_output_dir
+from config.logging_config import setup_logging
+
+# Configure logging for this script
+setup_logging()
+
 
 @task
-def save_data_to_Excel():
+def save_data_to_excel():
+    """
+    Saving the extracted articles on excel file.
+    """
+    logging.info("Starting saving articles on excel")
 
     # Define the path for the new Excel file in the output directory
     output_dir = Path(get_output_dir())
     excel_file_path = output_dir / "Articles.xlsx"
-    
+
     # Create a new Excel workbook and add a worksheet with the name 'Sheet1'
     workbook = excel.create_workbook(fmt="xlsx", sheet_name="Sheet1")
-    
-    
+
     # Append a row with column headers
     sheet_name = "Sheet1"
     worksheet = workbook.worksheet(sheet_name)
     row_to_append = [
-        ["No", "Title", "Date", "Description", "PictureFilename", "Count", "ContainsMoney"]
+        ["No", "Title", "Date", "Description",
+            "PictureFilename", "Count", "ContainsMoney"]
     ]
     # Append the row to the worksheet
     worksheet.append_rows_to_worksheet(row_to_append, header=False)
-    
+
     # worksheet = workbook.worksheet(sheet_name)
     workbook.save(excel_file_path)
-    headers = ["No", "Title", "Date", "Description", "PictureFilename", "Count", "ContainsMoney"]
-    print("inside save to excel function")
+
     try:
         # Fetch the created work items and write them to the Excel file
         for item in workitems.inputs:
@@ -52,7 +55,8 @@ def save_data_to_Excel():
             ]
             worksheet.append_rows_to_worksheet([row], header=False)
         workbook.save(excel_file_path)
-        print("workitems finished successfully")
+        logging.info("Articles Saved Sucessfully")
+
     except Exception as e:
-        print(e, "save item didn't work")
-        pass
+
+        logging.error('Save item did not work because of:- %s', e)
