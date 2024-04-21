c9687e1c93abac6db66da40777e56461c7a30a54
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-21 04:33:13 +0200

    Delete logging_config.py

--- logging_config.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-# Setting Up logging
-def setup_logging():
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
