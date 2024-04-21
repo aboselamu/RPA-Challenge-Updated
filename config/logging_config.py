3fd732d7399bcebc5c2338fefe6fb6c5b4834219
aboselamu <34035713+aboselamu@users.noreply.github.com>
2024-04-21 04:32:58 +0200

    Create logging_config.py

---
+++ config/logging_config.py
@@ -0,0 +1,6 @@
+import logging
+
+# Setting logging configuration 
+def setup_logging():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
